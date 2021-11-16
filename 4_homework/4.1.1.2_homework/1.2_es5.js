6188b8adee663af24836541365ad7f827fb284cf
AleksandrBukreev <alex.73@mail.ru>
2021-11-17 01:25:09 +0300

    create dz 1.2

---
+++ 4_homework/4.1.1.2_homework/1.2_es5.js
@@ -0,0 +1,41 @@
+// а) конструктор Post, который принимает параметры author, text, date и сохраняет
+// их как свойства объекта. Объекты типа Post должны иметь метод edit, который будет 
+// принимать текст и записывать его в свойство text объекта.
+// б) конструктор AttachedPost, который принимает параметры author, text, date.
+// Проинициализируйте эти свойства с помощью конструктора Post, чтобы не 
+// дублировать код. Также в конструкторе AttachedPost должно создаваться свойство
+// highlighted со значением false. Унаследуйте в объектах типа AttachedPost методы из Post.
+// Объекты типа AttachedPost должны иметь метод makeTextHighlighted,
+// который будет назначать свойству highlighted значение true.
+'use strict';
+
+function Post(author, text, date) {
+    this.author = author;
+    this.text = text;
+    this.date = date;
+}
+Post.prototype.edit = function (text) { //метод edit, который будет 
+    // принимать текст и записывать его в свойство text объекта
+    this.text = text;
+}
+const post_1 = new Post('alex', 'lorem lorem', '2021');
+console.log(post_1);
+post_1.edit('новый текст');
+console.log(post_1);
+
+function AttachedPost(author, text, date) {
+    Post.call(this, author, text, date);
+    this.highlighted = false;//свойство highlighted со значением false
+}
+AttachedPost.prototype = Object.create(Post.prototype);//создание прослойки create, что бы прототипы не указывали на одну область памяти 
+AttachedPost.prototype.constructor = AttachedPost;//из за строки выше утерян конструктор, прописываем его вручную
+
+AttachedPost.prototype.makeTextHighlighted = function () {
+    this.highlighted = true;//свойство highlighted со значением true
+}
+
+const attached_1 = new AttachedPost('admin', 'LOREM LOREM', '2022');
+console.log(attached_1);
+attached_1.makeTextHighlighted();
+attached_1.edit('НОВЫЙ ТЕКСТ');
+console.log(attached_1);
